e9578ba9a6763c94b99bab2acd35eb29812cabf2
Kidays10M <hello@kidays.fr>
2026-02-12 01:38:01 +0100

    Refactor config.php creation to use Config class
    Replaced config file creation with a class-based configuration structure.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,23 @@
 cd /var/www/html
 
 if [ ! -f config.php ]; then
-  cp config-sample.php config.php
+cat > config.php <<EOF
+<?php
 
-  sed -i "s/DB_HOST.*/DB_HOST', '${MYSQLHOST}');/" config.php
-  sed -i "s/DB_NAME.*/DB_NAME', '${MYSQLDATABASE}');/" config.php
-  sed -i "s/DB_USERNAME.*/DB_USERNAME', '${MYSQLUSER}');/" config.php
-  sed -i "s/DB_PASSWORD.*/DB_PASSWORD', '${MYSQLPASSWORD}');/" config.php
-  sed -i "s/DB_PORT.*/DB_PORT', '${MYSQLPORT}');/" config.php
-  sed -i "s|BASE_URL.*|BASE_URL', '${BASE_URL}');|" config.php
+class Config {
+
+    const BASE_URL = '${BASE_URL}';
+    const LANGUAGE = 'english';
+    const DEBUG_MODE = false;
+
+    const DB_HOST = '${MYSQLHOST}';
+    const DB_NAME = '${MYSQLDATABASE}';
+    const DB_USERNAME = '${MYSQLUSER}';
+    const DB_PASSWORD = '${MYSQLPASSWORD}';
+    const DB_PORT = '${MYSQLPORT}';
+
+}
+EOF
 fi
 
 php-fpm &
