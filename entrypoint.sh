2e163c6c098666785de99b1ea164f688fcd9f20f
Kidays10M <hello@kidays.fr>
2026-02-12 01:33:21 +0100

    Refactor config.php creation with sed commands
    Replaced the config.php generation with sed commands to update database configuration values.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,22 +3,14 @@
 cd /var/www/html
 
 if [ ! -f config.php ]; then
-cat > config.php <<EOF
-<?php
+  cp config-sample.php config.php
 
-class Config {
-
-    const BASE_URL = '${BASE_URL}';
-    const LANGUAGE = 'english';
-    const DEBUG_MODE = FALSE;
-
-    const DB_HOST = '${MYSQLHOST}';
-    const DB_NAME = '${MYSQLDATABASE}';
-    const DB_USERNAME = '${MYSQLUSER}';
-    const DB_PASSWORD = '${MYSQLPASSWORD}';
-    const DB_PORT = '${MYSQLPORT}';
-}
-EOF
+  sed -i "s/DB_HOST.*/DB_HOST', '${MYSQLHOST}');/" config.php
+  sed -i "s/DB_NAME.*/DB_NAME', '${MYSQLDATABASE}');/" config.php
+  sed -i "s/DB_USERNAME.*/DB_USERNAME', '${MYSQLUSER}');/" config.php
+  sed -i "s/DB_PASSWORD.*/DB_PASSWORD', '${MYSQLPASSWORD}');/" config.php
+  sed -i "s/DB_PORT.*/DB_PORT', '${MYSQLPORT}');/" config.php
+  sed -i "s|BASE_URL.*|BASE_URL', '${BASE_URL}');|" config.php
 fi
 
 php-fpm &
