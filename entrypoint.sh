6901725a841b2ede84366a74ac5fc9b7b061fc71
Kidays10M <hello@kidays.fr>
2026-02-12 02:07:28 +0100

    Enable PHP error reporting for debugging
    Add temporary PHP debugging configuration.

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,12 @@ EOF
 echo "================= GENERATED /var/www/html/config.php ================="
 nl -ba /var/www/html/config.php | sed -n '1,120p'
 echo "======================================================================"
+# Debug PHP (temporaire)
+echo "display_errors=1" > /usr/local/etc/php/conf.d/zz-debug.ini
+echo "display_startup_errors=1" >> /usr/local/etc/php/conf.d/zz-debug.ini
+echo "error_reporting=E_ALL" >> /usr/local/etc/php/conf.d/zz-debug.ini
+echo "log_errors=1" >> /usr/local/etc/php/conf.d/zz-debug.ini
+echo "error_log=/proc/self/fd/2" >> /usr/local/etc/php/conf.d/zz-debug.ini
 
 php-fpm &
 caddy run --config /etc/caddy/Caddyfile
